387b16ac45b3e7c1ecc7980db2abcb5b2d3f4bf0
p0rtalfan19 <dmitgomonov@gmail.com>
2025-01-19 14:34:10 +0300

    Test

---
+++ main.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# Устанавливаем соединение с базой данных
+connection = sqlite3.connect('recipes.db')
+cursor = connection.cursor()
+
+
+# Создаем таблицу Recipes
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Recipes (
+Name TEXT PRIMARY KEY,
+Receipt TEXT NOT NULL,
+Ingredients TEXT NOT NULL,
+meal_time TEXT NOT NULL
+)
+''')
+
+
+
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+ID Integer PRIMARY KEY,
+Name TEXT PRIMARY KEY,
+login TEXT NOT NULL,
+password TEXT NOT NULL,
+favorite TEXT
+)
+''')
+
+
+
+# Сохраняем изменения и закрываем соединение
+connection.commit()
+connection.close()
